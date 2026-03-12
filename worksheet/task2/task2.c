7f7d5c3b58269b0c7baba7f699b42fadcae2c301
SawyerCowley <xmgj0858@leeds.ac.uk>
2026-03-12 18:00:48 +0000

    worksheet

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -2,8 +2,8 @@
  * Sem 2, week 5
  *
  * Portfolio submission: Hexadecimal to Decimal Converter
- * Name:
- * ID: 
+ * Name: SAwyer Cowley
+ * ID: 201958891
  */
 
 #include <stdio.h>
@@ -17,7 +17,39 @@ int main(void){
 
 	// if input contains invalid hex digit
 	// printf("Error: Invalid Hexadecimal\n");
+	scanf("%s", hex);
 	
+	int i = 0;
+	while (hex[i] != '\0') {
+		//checks that all values must be allowed in hex
+        if (!((hex[i] >= '0' && hex[i] <= '9') || (hex[i] >= 'a' && hex[i] <= 'f') || (hex[i] >= 'A' && hex[i] <= 'F'))) {
+            printf("Invalid Hexadecimal\n");
+			//terminate
+			return 0;
+        }
+        i++;
+    }
+
+	//function to multiply digits by corresponding powers of 16
+	
+	//running product, reset i
+	int inter = 0;
+	i = 0;
+	//while the empty character hasnt been reached
+	while (hex[i] != '\0'){
+		if (hex[i] >= '0' && hex[i] <= '9')
+            inter = hex[i] - '0';
+        else if (hex[i] >= 'A' && hex[i] <= 'F')
+		//char in C is 'short int' so value of character can be subtracted to reach integer and + 10 (integer offset)
+            inter = hex[i] - 'A' + 10;
+        else if (hex[i] >= 'a' && hex[i] <= 'f')
+            inter = hex[i] - 'a' + 10;
+
+		//multiplication formula for hexedecimal digits
+		decimal = decimal * 16 + inter;
+		i++;
+	}
+
 	// print the decimal result
 	printf("decimal:%ld\n", decimal);
 	
